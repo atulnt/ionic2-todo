49c7eadc8a67a6e004980c000c7f41ca6d4ca5d0
atulnt <mahajan.atul@gmail.com>
2018-07-08 01:03:32 -0400

    Ionic-2-Todo App

---
+++ src/pages/add-item/add-item.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+/*import { IonicPageModule } from 'ionic-angular';
+import { AddItemPage } from './add-item';
+
+@NgModule({
+  declarations: [
+    AddItemPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AddItemPage),
+  ],
+})
+export class AddItemPageModule {}
+*/
